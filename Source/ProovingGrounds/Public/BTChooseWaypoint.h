079429609e9f6024f52cb5afbf40724f24e37b68
Daniil <sf.kro.gm@gmail.com>
2018-05-06 17:32:28 +0300

    Some BT in blueprints and start of C++ BT

---
+++ Source/ProovingGrounds/Public/BTChooseWaypoint.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTChooseWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROOVINGGROUNDS_API UBTChooseWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
