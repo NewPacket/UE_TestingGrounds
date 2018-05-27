e92a27712efe36d0e7d9aa1ab41ec9a5f6d3610e
Daniil <sf.kro.gm@gmail.com>
2018-05-27 13:44:02 +0300

    Ai simple patrol

--- Source/ProovingGrounds/Private/BTChooseWaypoint.cpp
+++ Source/ProovingGrounds/Private/BTChooseWaypoint.cpp
@@ -1,10 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BTChooseWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "TP_ThirdPerson/PatrollingGuard.h"
 
 EBTNodeResult::Type UBTChooseWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	
+	auto blackboardComp = OwnerComp.GetBlackboardComponent();
+	auto index = blackboardComp->GetValueAsInt(selectedIndex.SelectedKeyName);
+	auto owner = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
+	if (owner)
+	{
+		blackboardComp->SetValueAsObject(keySelector.SelectedKeyName, owner->patrolPoints[index]);
+		auto nextIndex = (index + 1) % owner->patrolPoints.Num();
+		blackboardComp->SetValueAsInt(selectedIndex.SelectedKeyName, nextIndex);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("cant find owner"))
+	}
 	return EBTNodeResult::Succeeded;
 }
 
